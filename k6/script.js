6f4ae465ea3af05507980132e3ad2331da6a3d55
hatena75 <s2330157@u.tsukuba.ac.jp>
2024-05-30 17:04:12 +0000

    add k6 files

---
+++ k6/script.js
@@ -0,0 +1,27 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  // A number specifying the number of VUs to run concurrently.
+  //vus: 11,
+  // A string specifying the total duration of the test run.
+  duration: '10s',
+  // curl corresponding -k
+  insecureSkipTLSVerify: true,
+};
+
+// The function that defines VU logic.
+//
+// See https://grafana.com/docs/k6/latest/examples/get-started-with-k6/ to learn more
+// about authoring k6 scripts.
+//
+
+//curl -H "Content-Type: application/json" -d '@attested.json' --cacert ./configuration-default/certificates/qvs-cert.pem -k https://localhost:8799/attestation/sgx/dcap/v1/report
+
+const url = 'https://localhost:8799/attestation/sgx/dcap/v1/report';
+//const cert = open('../configuration-default/certificates/qvs-cert.pem');
+const quote = open('../attested.json');
+
+export default function() {
+  http.post(url, quote, { headers: { 'Content-Type': 'application/json' } });
+}
